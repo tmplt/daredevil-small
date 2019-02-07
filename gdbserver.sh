3e806037733deeffa85e0292b0e48827ffe22429
Tmplt <tmplt@dragons.rocks>
2019-02-07 19:09:43 +0100

    feat(gdbserver): rewrite to match S32 SDK workflow
    Exception being that ./gdbserver.sh is not in single-run mode; it runs
    continously waiting for gdb clients. ./.gdbinit has been rewritten by to
    mirror the debug output of board programming in the S32 SDK. The biggest
    changes is likely that breakpoints are now cleared, and semihosting
    is enabled (not that we need it, but we want to mirror SDK behavior).

--- gdbserver.sh
+++ gdbserver.sh
@@ -1,1 +1,1 @@
-JLinkGDBServerCLExe -select USB -device S32K144 -if SWD -speed 4000 -ir
+JLinkGDBServerCLExe -select USB -device S32K144 -if SWD -speed 4000 -ir -endian little -vd -ir -localhostonly 1 -strict -timeout 0 -nogui
