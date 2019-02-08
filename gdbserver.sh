17edbceef503ea57fe3e8ee637905b4b59a9b039
Tmplt <tmplt@dragons.rocks>
2019-02-08 15:47:00 +0100

    fix(gdbserver): copy JLink options verbatim from S32 SDK
    With these set correct, error output and all, the CAN-FD example
    seemingly programs without issues.

--- gdbserver.sh
+++ gdbserver.sh
@@ -1,1 +1,3 @@
-JLinkGDBServerCLExe -select USB -device S32K144 -if SWD -speed 4000 -ir -endian little -vd -ir -localhostonly 1 -strict -timeout 0 -nogui
+#!/usr/bin/env bash
+# TODO: mass erase device and program examples/nxp-flexcanfd.rs with this.
+JLinkGDBServerCLExe -if jtag -device S32K144 -endian little -speed 1000 -port 2331 -swoport 2332 -telnetport 2333 -vd -ir -localhostonly 1 -strict -timeout 0 -nogui
