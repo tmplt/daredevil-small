542a5df5867976925a386076bd08d82fbb11f8d7
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2019-02-12 13:16:45 +0100

    Comment/replace magical bit string in nxp-flexcan

--- examples/nxp-flexcan.rs
+++ examples/nxp-flexcan.rs
@@ -22,47 +22,66 @@ unsafe fn main() -> ! {
     let p = s32k144::Peripherals::take().unwrap();
 
     // Disable watchdog
-    p.WDOG.cnt.write(|w| w.bits(0xd928c520)); // unlock
+    p.WDOG.cnt.write(|w| w.bits(0xd928c520)); // unlock, magical number
     p.WDOG.toval.write(|w| w.bits(0xffff)); // maximum timeout value
-    p.WDOG.cs.write(|w| w.bits(0x2100)); // disable watchdod
+    p.WDOG.cs.write(|w| {
+        w.en()._0() // Disable Watchdog
+            .clk().bits(0b01) // Set Watchdog clock to LPO
+            .cmd32en()._1() // Enable 32-bit refresh/unlock support
+    });
 
     // SOSC_init_8Mhz
-    p.SCG.soscdiv.write(|w| w.bits(0x101));
-    p.SCG.sosccfg.write(|w| w.bits(0x24));
+    p.SCG.soscdiv.write(|w| {
+        w.soscdiv1().bits(0b001) // Divide by 1
+            .soscdiv2().bits(0b001) // Divide by 1
+    });
+    p.SCG.sosccfg.write(|w| {
+        w.bits(0x24) // XXX: Bit 6 is unused in this field.
+        //w.hgo()._1() // Set crystal oscillator for high gain
+    });
     while p.SCG.sosccsr.read().lk().is_1() {} // Ensure SOSCCSR unlocked
-    p.SCG.sosccsr.write(|w| w.bits(0x1));
-    while p.SCG.sosccsr.read().soscvld().is_1() {}
+    p.SCG.sosccsr.write(|w| w.soscen()._1());
+    while p.SCG.sosccsr.read().soscvld().is_1() {} // Validate enabling of SysOSC
 
     // SPLL_init_160Mhz
-    while p.SCG.spllcsr.read().lk().is_1() {}
-    p.SCG.spllcsr.write(|w| w.bits(0x0));
-    p.SCG.splldiv.write(|w| w.bits(0x302));
-    p.SCG.spllcfg.write(|w| w.bits(0x180000));
-    while p.SCG.spllcsr.read().lk().is_1() {}
-    p.SCG.spllcsr.write(|w| w.bits(0x1));
+    while p.SCG.spllcsr.read().lk().is_1() {} // Ensure the System Phase-locked loop is unlocked
+    p.SCG.spllcsr.write(|w| w.spllen()._0()); // Disable SPLL
+    p.SCG.splldiv.write(|w| {
+        w.splldiv1().bits(0b010) // Divide by 2
+            .splldiv2().bits(0b011) // Divide by 4
+    });
+    p.SCG.spllcfg.write(|w| w.mult().bits(0b11000)); // Muliply factor 40
+    while p.SCG.spllcsr.read().lk().is_1() {} // Ensure Control Status Register is unlocked
+    p.SCG.spllcsr.write(|w| w.spllen()._1()); // Enable CSR
     while p.SCG.spllcsr.read().spllvld().is_1() {}
 
     // NormalRUNmode_80Mhz
     p.SCG.rccr.modify(|_, w| {
-        w.scs().bits(6)
-         .divcore().bits(0b01)
-         .divbus().bits(0b01)
-         .divslow().bits(0b10)
+        w.scs().bits(6) // System PLL
+         .divcore().bits(0b01) // Divide Core Clock by 2
+         .divbus().bits(0b01) // Divide Bus Clock by 2
+         .divslow().bits(0b10) // Divide Slow Clock by 3
     });
-    while p.SCG.csr.read().scs().bits() != 6 {} // wait while clock is changed
+    while p.SCG.csr.read().scs().bits() != 6 {} // Ensure clock is configured to System PLL
 
 
     // FLEXCAN0_init
-    p.PCC.pcc_flex_can0.modify(|_, w| w.cgc()._1());
-    p.CAN0.mcr.modify(|_, w| w.mdis()._1());
-    p.CAN0.ctrl1.modify(|_, w| w.clksrc()._0());
+    p.PCC.pcc_flex_can0.modify(|_, w| w.cgc()._1()); // Enable clock for CAN
+    p.CAN0.mcr.modify(|_, w| w.mdis()._1()); // Disable FlexCAN module
+    p.CAN0.ctrl1.modify(|_, w| w.clksrc()._0()); // Set oscillator clock to be CAN engine clock
     p.CAN0.mcr.modify(|_, w| {
-        w.mdis()._0()
-         .frz()._1()
-         .halt()._1()
+        w.mdis()._0() // Reenable FlexCAN
+         .frz()._1() // Enable Freeze Mode
+         .halt()._1() // Enter freeze mode if FRZ bit is asserted
     });
     while p.CAN0.mcr.read().frzack().is_1() {}
-    p.CAN0.ctrl1.write(|w| w.bits(0xdb0006));
+    //p.CAN0.ctrl1.write(|w| w.bits(0xdb0006));
+    p.CAN0.ctrl1.modify(|_, w| {
+        w.propseg().bits(0b110) // propagation segment time = (PROPSEG+1) * one Sclock period
+            .pseg2().bits(0b011) // length of phase segment 2
+            .pseg1().bits(0b011) // length of phase segment 1
+            .rjw().bits(0b11)   // Resync Jump Width
+    });
     // for i in 0..128 {
     //     p.CAN0.embedded_ram[i].write(|w| w.bits(0));
     // }
@@ -76,17 +95,17 @@ unsafe fn main() -> ! {
     p.CAN0.embedded_ram[4 * 4 + 1].modify(|_, w| {
         w.bits(0x14440000)
     });
-    p.CAN0.mcr.write(|w| w.bits(0x1f));
+    p.CAN0.mcr.write(|w| w.maxmb().bits(0x1f)); // Sets the number of the last message buffer (4*5 = 20MB)
     while p.CAN0.mcr.read().frzack().is_1() {}
     while !p.CAN0.mcr.read().notrdy().is_0() {}
 
     // PORT_init
-    p.PCC.pcc_porte.modify(|_, w| w.cgc()._1());
-    p.PORTE.pcr4.modify(|_, w| w.mux()._101());
-    p.PORTE.pcr5.modify(|_, w| w.mux()._101());
-    p.PCC.pcc_portd.modify(|_, w| w.cgc()._1());
-    p.PORTD.pcr16.write(|w| w.bits(0x100));
-    p.PTD.pddr.modify(|_, w| w.bits(1 << 16));
+    p.PCC.pcc_porte.modify(|_, w| w.cgc()._1()); // Enable PortE
+    p.PORTE.pcr4.modify(|_, w| w.mux()._101()); // Maybe enable CAN RX
+    p.PORTE.pcr5.modify(|_, w| w.mux()._101()); // Maybe enable CAN TX
+    p.PCC.pcc_portd.modify(|_, w| w.cgc()._1()); // Enable PortD
+    p.PORTD.pcr16.write(|w| w.bits(0x100)); // Maybe enable Green LED
+    p.PTD.pddr.modify(|_, w| w.bits(1 << 16)); // Configure pin 16 to be output
 
     // just transmit messages in a loop down here
     loop {
