ffacc68dddcfa4ef72f7be180c94b0ef11fc6496
Tmplt <tmplt@dragons.rocks>
2019-03-04 18:38:00 +0100

    refactor: split CAN-FD code into separate module

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,20 @@ use rtfm::{app, Instant};
 use s32k144::Interrupt;
 use s32k144evb::wdog;
 
+pub mod can;
 pub mod csec;
 pub mod panic;
 pub mod utils;
 
 const MAGIC_SCALAR: u32 = 5000 / 0xfff;
-const MSG_BUF_SIZE: usize = 18;
 const PERIOD: u32 = 8_000;
 
 #[app(device = s32k144)]
 const APP: () = {
     // Resources
     static mut ADC: s32k144::ADC0 = ();
-    static mut CAN: s32k144::CAN0 = ();
     static mut CSEc: csec::CSEc = ();
+    static mut CAN: can::CAN = ();
 
     #[init(schedule = [poll_sensor])]
     fn init() {
@@ -34,9 +34,9 @@ const APP: () = {
         configure_spll_clock(&device.SCG);
 
         adc_init(&device.PCC, &device.ADC0);
-        flexcan0_init(
+        let can = can::CAN::init(
             &device.PCC,
-            &device.CAN0,
+            device.CAN0,
             &device.PORTE,
             &device.PORTD,
             &device.PTD,
@@ -56,7 +56,7 @@ const APP: () = {
             .unwrap();
 
         ADC = device.ADC0;
-        CAN = device.CAN0;
+        CAN = can;
         CSEc = csec;
     }
 
@@ -95,7 +95,7 @@ const APP: () = {
 
         // Transmit the payload, with a prefixed initialization vector.
         payload[..16].clone_from_slice(&init_vec);
-        transmit_over_can(&can, &mut payload);
+        can.transmit(&payload);
 
         schedule.poll_sensor(scheduled + PERIOD.cycles()).unwrap();
     }
@@ -162,108 +162,8 @@ fn adc_init(pcc: &s32k144::PCC, adc: &s32k144::ADC0) {
     }
 }
 
-fn flexcan0_init(
-    pcc: &s32k144::PCC,
-    can: &s32k144::CAN0,
-    porte: &s32k144::PORTE,
-    portd: &s32k144::PORTD,
-    ptd: &s32k144::PTD,
-) {
-    unsafe {
-        pcc.pcc_flex_can0.modify(|_, w| w.cgc()._1());
-        can.mcr.modify(|_, w| w.mdis()._1());
-        can.ctrl1.modify(|_, w| w.clksrc()._1());
-        can.mcr.modify(|_, w| w.mdis()._0());
-        while can.mcr.read().frzack().is_0() {}
-
-        // Configure nominal phase
-        #[rustfmt::skip]
-        can.cbt.write(|w| {
-            w.epseg2().bits(0b0_1111) // Bit length of phase segment 2
-                .epseg1().bits(0b0_1111) // Bit length of phase segment 2
-                .epropseg().bits(0b10_1110) // Bit time length of propagation segment
-                .erjw().bits(0b0_1111) // Extended Resync Jump Width
-                .epresdiv().bits(0b00_0000_0001) // Ratio between PE clock and Sclock
-                .btf()._1() // Enable bit timing format
-        });
-
-        // Configure data phase
-        #[rustfmt::skip]
-        can.fdcbt.write(|w| {
-            w.fpseg2().bits(0b011) // Bit time length of Fast Phase Segment 2
-                .fpseg1().bits(0b111) // Bit time length of Fast Phase Segment 1
-                .fpropseg().bits(0b0_0111) // Bit time length of the propagation segment
-                .frjw().bits(0b011) // Number of time quanta per resynchronization
-                .fpresdiv().bits(0b00_0000_0001) // Ratio between PE clock and Sclock
-        });
-        #[rustfmt::skip]
-        can.fdctrl.write(|w| {
-            w.tdcoff().bits(0b1_1111) // Transceiver Delay Compensation Offset
-                .tdcen()._1() // Enable TDC
-                .mbdsr0()._11() // 64 bytes per message buffer
-                .fdrate()._1() // Enable Bit Rate Switch
-        });
-
-        // Clear 128 words RAM in module
-        for i in 0..128 {
-            can.embedded_ram[i].write(|w| w.bits(0));
-        }
-        // init CAN0 16 msg buf filters
-        for i in 0..16 {
-            can.rximr[i].write(|w| w.bits(0xFFFFFFFF));
-        }
-
-        // Global acceptance mask: check all ID bits
-        can.rxmgmask.write(|w| w.bits(0x1FFFFFFF));
-
-        // Message Buffer 4 - receive setup
-        can.embedded_ram[(4 * MSG_BUF_SIZE) + 0].modify(|_, w| w.bits(0xC4000000));
-        // Msg buf 4, word 1: standard ID = 0x511
-        can.embedded_ram[(4 * MSG_BUF_SIZE) + 1].modify(|_, w| w.bits(0x14440000));
-
-        // Enable CRC fix for ISO CAN FD
-        can.ctrl2.modify(|_, w| w.isocanfden()._1());
-
-        // Negate FlexCAN 1 halt state & enable CAN FD for 32 MBs
-        can.mcr.write(|w| w.bits(0x0000081F));
-
-        // Wait for FRZACK to clear and module ready
-        while can.mcr.read().frzack().is_1() {}
-        while !can.mcr.read().notrdy().is_0() {}
-    }
-
-    // Initialize CAN ports, and light green LED
-    unsafe {
-        pcc.pcc_porte.modify(|_, w| w.cgc()._1());
-        porte.pcr4.modify(|_, w| w.mux()._101());
-        porte.pcr5.modify(|_, w| w.mux()._101());
-        pcc.pcc_portd.modify(|_, w| w.cgc()._1());
-        portd.pcr16.write(|w| w.bits(0x100));
-        ptd.pddr.modify(|_, w| w.bits(1 << 16));
-    }
-}
-
 fn split_u16_to_byte_array(n: u16) -> [u8; 2] {
     let x: u8 = ((n >> 8) & 0xff) as u8;
     let y: u8 = (n & 0xff) as u8;
     [x, y]
-}
-
-fn byte_array_to_u32(array: &mut [u8]) -> u32 {
-    let mut x: u32 = (array[3] as u32) << 24;
-    x += (array[2] as u32) << 16;
-    x += (array[1] as u32) << 8;
-    x += array[0] as u32;
-    x
-}
-
-fn transmit_over_can(can: &s32k144::CAN0, payload: &mut [u8]) {
-    // FLEXCAN0_transmit_msg
-    unsafe {
-        can.iflag1.write(|w| w.bits(0x1));
-        can.embedded_ram[(0 * MSG_BUF_SIZE) + 2].write(|w| w.bits(0xa5112233));
-        can.embedded_ram[(0 * MSG_BUF_SIZE) + 3].write(|w| w.bits(byte_array_to_u32(payload)));
-        can.embedded_ram[(0 * MSG_BUF_SIZE) + 1].write(|w| w.bits(0x15540000));
-        can.embedded_ram[(0 * MSG_BUF_SIZE) + 0].write(|w| w.bits(0xcc4f0000 | (8 << 16)));
-    }
 }
